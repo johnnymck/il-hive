310730abf0f9a6f1194fb808c2a3b9c8814ee286
Johnny McKenzie <iamd2204@gmail.com>
2023-07-27 16:08:13 +0100

    created new layer for comms with Mzinga

---
+++ uhplayer.py
@@ -0,0 +1,38 @@
+"""UhpLayer.py
+Glue library for interfacing agents with Mzinga Viewer https://github.com/jonthysell/Mzinga using Universal Hive Protocol (UHP)
+"""
+
+import hive
+
+class UhpLayer(object):
+    def __init__(self, agent):
+        self.agent = agent
+    
+    def repl(self):
+        #read
+        x = input()
+        #eval
+        output = None
+        if x == 'GameTypeString':
+            output = self.game_string()
+        elif x == 'GameStateString':
+            output = self.game_state_string()
+        #print
+        print(output)
+        print('ok')
+
+    def game_state_string(self):
+        if self.agent.board.winner == None:
+            return 'InProgress'
+        elif self.agent.board.winner == False:
+            return 'Draw'
+        else:
+            if self.agent.board.winner() == hive.Color.Black:
+                return 'BlackWins'
+            elif self.agent.board.winner() == hive.Color.White:
+                return 'WhiteWins'
+            else:
+                raise Exception()
+    
+    def game_string(self):
+        return 'Base+MLP'

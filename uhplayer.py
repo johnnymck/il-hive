aba54e209f75b6091128dab7c335fd871c2c65b3
Johnny McKenzie <iamd2204@gmail.com>
2023-07-29 14:38:05 +0100

    tidied up the gen_valid_moves method

--- uhplayer.py
+++ uhplayer.py
@@ -42,3 +42,17 @@ class UhpLayer(object):
     
     def game_string(self):
         return 'Base+MLP'
+
+    def gen_valid_moves(self):
+        in_play = self.agent.board.pieces_in_play
+        if not in_play:
+            if self.agent.color == hive.Color.White:
+                return 'wA1;wB1;wS1;wG1;wP;wM;wL'
+            else:
+                return 'bA1;bB1;bS1;bG1;bP;bM;bL'
+        else:
+            moves = [str(i) for i in self.agent.board.free_pieces(self.agent.color)]
+            return ';'.join(moves)
+    
+    def best_move(self):
+        return 

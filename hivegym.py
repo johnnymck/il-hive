55ffd51598ee7414ef191a2a3f1904a479a83856
Johnny McKenzie <iamd2204@gmail.com>
2023-08-07 17:21:57 +0100

    initial hive gym added

---
+++ hivegym.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+import numpy as np
+import hive
+
+from gymnasium import spaces
+from stable_baselines3.common.env_checker import check_env
+
+class HiveEnv(gym.Env):
+    super().__init__()
+
+
+if __name__ == '__main__':
+    hivegym = HiveEnv()
+    check_env(hivegym)

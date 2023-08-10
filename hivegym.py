3f46cdadac58bfce902e66bee1f0939f7d42f1fa
Johnny McKenzie <iamd2204@gmail.com>
2023-08-10 11:44:56 +0100

    didn't commit for some reason

--- hivegym.py
+++ hivegym.py
@@ -7,6 +7,7 @@ from stable_baselines3.common.env_checker import check_env
 
 class HiveEnv(gym.Env):
     """Hive game Environment that implements StableBaselines3's gym interface."""
+    def __init__(self):
         super().__init__()
 
 
